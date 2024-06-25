9a7f95d5051b6a69410dc46c43cc34e48c582265
Marwan Al-Obaidi <marwan1alobaidi@live.fi>
2024-06-25 15:42:27 +0300

    React Router topic added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
-import Home from './views/home';
+import Home from './views/home/home';
+import NavBar from './components/navBar/navBar';
+import ReactRouter from './views/reactJs/reactRouter/reactRouter';
 
 function App() {
   return (
     <Router>
+      <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/*" element={<p>Page not found</p>} />
+        <Route path="/react-router" element={<ReactRouter />} />
       </Routes>
     </Router>
   );
---
+++ src/views/reactJs/reactRouter/reactRouter.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ReactRouter() {
+    return (
+        <div>
+            <h1>React Router</h1>
+        </div>
+    );
+}
+
+export default ReactRouter;
