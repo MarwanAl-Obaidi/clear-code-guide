8aaa8771b7fe1830a8a8969620c03553cdd877b6
Marwan Al-Obaidi <marwan1alobaidi@live.fi>
2024-06-25 16:14:19 +0300

    CodeBlocks implemented

--- src/views/reactJs/reactRouter/reactRouter.js
+++ src/views/reactJs/reactRouter/reactRouter.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import CodeBlock from '../../../components/codeBlock/codeBlock';
 
 function ReactRouter() {
+
     return (
         <div>
             <h1>React Router</h1>
             <p>React Router is a standard library for routing in React.</p>
+            <CodeBlock>
+                {`npm install react-router-dom`}
+            </CodeBlock>
+            <CodeBlock language="JavaScript">
+                {``}
+            </CodeBlock>
         </div>
     );
 }
