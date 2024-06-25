ad85d2746f6fd98379335d3c1d007f828022643d
Marwan Al-Obaidi <marwan1alobaidi@live.fi>
2024-06-25 13:34:43 +0300

    Navigation Bar implemented

---
+++ src/components/navBar/navBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const NavBar = () => {
+    return (
+        <nav>
+            <ul>
+                <li><a href="#">Page One</a></li>
+                <li><a href="#">Page Two</a></li>
+            </ul>
+        </nav>
+    );
+};
+
+export default NavBar;
