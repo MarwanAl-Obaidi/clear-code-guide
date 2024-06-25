cee1464766a4c89f5590cae10f03abfc1a0247bb
Marwan Al-Obaidi <marwan1alobaidi@live.fi>
2024-06-25 13:18:53 +0300

    Preliminary homepage set up

---
+++ src/views/home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Home() {
+    return (
+        <div>
+            <h1>Welcome to Clear Code Guide</h1>
+            <p>This is a site made by Marwan Al-Obaidi and it is meant to be a an alternate source for programming concepts that other guides simply do not explain properly.</p>
+            <p>The site is currently under contruction and many parts will be broken.</p>
+        </div>
+    );
+}
+
+export default Home;
