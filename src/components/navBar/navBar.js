db0f1928a933c6442ec05de9623733e91789661b
Marwan Al-Obaidi <marwan1alobaidi@live.fi>
2024-06-25 15:57:33 +0300

    CSS for Navbar implemented

--- src/components/navBar/navBar.js
+++ src/components/navBar/navBar.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './navBar.css';
 import { Link } from 'react-router-dom';
 
 const NavBar = () => {
     return (
-        <nav>
+        <nav className='navbar'>
             <ul>
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/React-JS">React JS</Link></li>
