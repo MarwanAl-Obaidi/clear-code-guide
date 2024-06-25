9472572db7d2ca4d7309d0c1d9287c421f159bd1
Marwan Al-Obaidi <marwan1alobaidi@live.fi>
2024-06-25 16:51:05 +0300

    Code block are now clearly shown

--- src/components/codeBlock/codeBlock.js
+++ src/components/codeBlock/codeBlock.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { coy } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import { vscDarkPlus } from 'react-syntax-highlighter/dist/esm/styles/prism';
 
 const CodeBlock = ({ language, children }) => {
     return (
-        <SyntaxHighlighter language={language} style={coy}>
+        <SyntaxHighlighter language={language} style={vscDarkPlus} wrapLongLines={true}>
             {children}
         </SyntaxHighlighter>
     );
--- src/views/reactJs/reactRouter/reactRouter.js
+++ src/views/reactJs/reactRouter/reactRouter.js
@@ -3,18 +3,33 @@ import CodeBlock from '../../../components/codeBlock/codeBlock';
 
 function ReactRouter() {
 
-    return (
-        <div>
-            <h1>React Router</h1>
-            <p>React Router is a standard library for routing in React.</p>
-            <CodeBlock>
-                {`npm install react-router-dom`}
-            </CodeBlock>
-            <CodeBlock language="JavaScript">
-                {``}
-            </CodeBlock>
-        </div>
-    );
+  return (
+    <div>
+      <h1>React Router</h1>
+
+      <CodeBlock>
+        {`npm install react-router-dom`}
+      </CodeBlock>
+      <CodeBlock language="javascript">
+        {`import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './views/home/home';
+import About from './views/about/about';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;`}
+      </CodeBlock>
+    </div>
+  );
 }
 
 export default ReactRouter;
