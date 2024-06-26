201813d09dd841c304af53d2b82e4f7a7efcb511
Marwan Al-Obaidi <marwan1alobaidi@live.fi>
2024-06-26 17:47:54 +0300

    Further additions to React Router topic

--- src/components/codeBlock/codeBlock.js
+++ src/components/codeBlock/codeBlock.js
@@ -4,7 +4,11 @@ import { vscDarkPlus } from 'react-syntax-highlighter/dist/esm/styles/prism';
 
 const CodeBlock = ({ language, children }) => {
     return (
-        <SyntaxHighlighter language={language} style={vscDarkPlus} wrapLongLines={true}>
+        <SyntaxHighlighter
+            language={language}
+            style={vscDarkPlus}
+            wrapLongLines={true}
+            showLineNumbers={true}>
             {children}
         </SyntaxHighlighter>
     );
--- src/views/reactJs/reactRouter/reactRouter.js
+++ src/views/reactJs/reactRouter/reactRouter.js
@@ -6,27 +6,64 @@ function ReactRouter() {
   return (
     <div>
       <h1>React Router</h1>
-
+      <p>React Router is a library for React applications that enables navigation between different components. It allows you to define routes in your application, mapping specific URLs to the corresponding React components that should render when those URLs are matched.</p>
+      <h2>Example implementation</h2>
+      <p>This example shows the most basic implementation of React Router. It includes a Home.js and About.js views that the user will be able to navigate between. The App.js will have the routing implemented.</p>
+      <h3>npm command</h3>
       <CodeBlock>
         {`npm install react-router-dom`}
       </CodeBlock>
+      <h3>Files</h3>
+      <h4>App.js</h4>
       <CodeBlock language="javascript">
         {`import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './views/home/home';
-import About from './views/about/about';
+import Home from './home';
+import About from './about';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+        <Route path="/" element={<Home />} /> // localhost:3000d
+        <Route path="/about" element={<About />} /> // localhost:3000/about
       </Routes>
     </Router>
   );
 }
 
 export default App;`}
+      </CodeBlock>
+      <h4>Home.js</h4>
+      <CodeBlock language="javascript">
+        {`import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+      <Link to="/about">About</Link>
+    </div>
+  );
+}
+
+export default Home;`}
+      </CodeBlock>
+      <h4>About.js</h4>
+      <CodeBlock language="javascript">
+        {`import React from 'react';
+import { Link } from 'react-router-dom';
+
+function About() {
+  return (
+    <div>
+      <h1>About</h1>
+      <Link to="/">Home</Link>
+    </div>
+  );
+}
+
+export default About;`}
       </CodeBlock>
     </div>
   );
